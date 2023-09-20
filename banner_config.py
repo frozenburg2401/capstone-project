55caf9cf4feb4480a54ab5416f5a42168cb5c653
frozenburg2401 <115753000+frozenburg2401@users.noreply.github.com>
2023-09-20 13:13:46 -0400

    Add files via upload

--- banner_config.py
+++ banner_config.py
@@ -3,38 +3,38 @@ from netmiko import ConnectHandler
 import time
 
 MLS1 = {
-    'device_type': 'extreme_exos'
-    'host': '205.169.134.1'
-    'username': 'admin'
-    'password': ''
+    "device_type": "extreme_exos"
+    "host": "205.169.134.1"
+    "username": "admin"
+    "password": ""
 }
 
 MLS2 = {
-    'device_type': 'extreme_exos'
-    'host': '205.169.130.1'
-    'username': 'admin'
-    'password': ''
+    "device_type": "extreme_exos"
+    "host": "205.169.130.1"
+    "username": "admin"
+    "password": ""
 }
 
 MLS3 = {
-    'device_type': 'extreme_exos'
-    'host': '205.169.132.1'
-    'username': 'admin'
-    'password': ''
+    "device_type": "extreme_exos"
+    "host": "205.169.132.1"
+    "username": "admin"
+    "password": ""
 }
 
 R1 = {
-    'device_type': 'extreme_exos'
-    'host': '205.169.64.5'
-    'username': 'vyos'
-    'password': 'vyos'
+    "device_type": "extreme_exos"
+    "host": "205.169.64.5"
+    "username": "vyos"
+    "password": "vyos"
 }
 
 R2 = {
-    'device_type': 'extreme_exos'
-    'host': '205.169.64.9'
-    'username': 'vyos'
-    'password': 'vyos'
+    "device_type": "extreme_exos"
+    "host": "205.169.64.9"
+    "username": "vyos"
+    "password": "vyos"
 }
 
 netcon = ConnectHandler(**MLS1)
