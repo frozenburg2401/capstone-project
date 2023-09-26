e258ba22398fe808e06a801e940b12e1d848fb57
frozenburg2401 <115753000+frozenburg2401@users.noreply.github.com>
2023-09-26 18:23:57 -0400

    added vyos banner setup

--- banner_config_v2.py
+++ banner_config_v2.py
@@ -2,11 +2,11 @@ import paramiko
 from netmiko import ConnectHandler
 
 #Note: ExOS specifies a limit on the size of the banner to be not more than 79 columns wide and 24 rows long
-BANNER="This is a test for the banner script\n\n"
+BANNER="This is a test for the banner script"
 
 exos_cmdlist="configure banner\n" + BANNER
 
-vyos_cmd="show config"
+vyos_cmd="set system login banner pre-login " + BANNER
 
 MLS1 = {
     "device_type": "extreme_exos",
@@ -47,12 +47,18 @@ for device in (MLS1, MLS2, MLS3, R1, R2):
     netcon = ConnectHandler(**device)
     print(netcon.find_prompt())
     if device["device_type"] == "extreme_exos":
-        print(netcon.send_command(exos_cmdlist))
-        netcon.send_command("show banner")
-        netcon.send_command("save configuration")
-    if device["device_type"] == "vyos_ssh":
-#        print(netcon.send_command(vyos_cmd))
+        #commenting out for now, cant figure out why this wont work
+        '''
+        output = netcon.send_command(exos_cmdlist)
+        output += netcon.send_command("show banner")
+        output += netcon.send_command("save configuration")
+        print(output)
+        '''
         pass
+    if device["device_type"] == "vyos_ssh":
+        output = netcon.send_command_set(vyos_cmd)
+        output += netcon.commit()
+        print(output)
     netcon.disconnect()
 
 print("script finished, exiting")
