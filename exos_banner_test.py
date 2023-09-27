e4172f391d1c1ab86a2d710fe24ca2aa5cf45cc6
frozenburg2401 <115753000+frozenburg2401@users.noreply.github.com>
2023-09-26 20:31:19 -0400

    Add files via upload

--- exos_banner_test.py
+++ exos_banner_test.py
@@ -16,7 +16,9 @@ device = {
 }
 
 netcon = ConnectHandler(**device)
-output = netcon.send_command("\n")
+#long story short, cli prompts get disabled on initialization of the connection
+#this really messes with the banner commands, reenabling it might fix it?
+output += netcon.send_command("enable cli paging")
 output += netcon.send_command("configure banner" + exos_banner)
 output += netcon.send_command("show banner")
 output += netcon.send_command("save configuration")
